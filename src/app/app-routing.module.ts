988238503d33d77ba4cacf5cfe8d831b5384bbd0
DeboBastian <cursosdeprogramacion00@gmail.com>
2023-04-01 17:11:18 +0200

    Navbar - falta boton search responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './components/characters/characters.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: 'full', redirectTo: '/movies' },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: '**', redirectTo:"/movies"}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule,
